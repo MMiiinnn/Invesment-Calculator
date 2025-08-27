c68499efc8bf70ed0e11d807b9692ebe01216992
Nghia Nguyen <minmin272610@gmail.com>
2025-08-27 17:24:16 +0700

    fix logo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
+import logo from "../assets/investment-calculator-logo.png";
+
 function Header() {
   return (
     <div id="header">
-      <img
-        src="investment-calculator-logo.png"
-        alt="Invesment Calculator Image"
-      />
+      <img src={logo} alt="Invesment Calculator Image" />
       <h1>Investment Calculator</h1>
     </div>
   );
